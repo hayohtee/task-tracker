dd5a94421f437f3d3eb38cd2afa60835b39b7244
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-18 12:04:30 +0100

    Add TaskList

--- internal/data/task.go
+++ internal/data/task.go
@@ -1,5 +1,7 @@
 package data
 
+import "time"
+
 // status is a custom type which represents the
 // status of a task.
 // It uses a string as the underlying type
@@ -10,3 +12,13 @@ const (
 	StatusInProgress = status("in-progress")
 	StatusDone       = status("done")
 )
+
+// task is a struct that holds all the information
+// about a particular task.
+type task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      status    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
