3bd47b434352d7057ca820519efe86e1f8ee1a5a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-18 12:22:14 +0100

    Add Get method

--- internal/data/task.go
+++ internal/data/task.go
@@ -1,6 +1,11 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"time"
+)
 
 // status is a custom type which represents the
 // status of a task.
@@ -21,4 +26,29 @@ type task struct {
 	Status      status    `json:"status"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+// TaskList is a custom type with the underlying data type of
+// []task.
+// It contains various method for creating and managing tasks.
+type TaskList []task
+
+// Get opens the provided file name, retrieve its JSON content and
+// decode it into TaskList.
+func (t *TaskList) Get(filename string) error {
+	fileContent, err := os.ReadFile(filename)
+	if err != nil {
+		switch {
+		case errors.Is(err, os.ErrNotExist):
+			return nil
+		default:
+			return err
+		}
+	}
+
+	if len(fileContent) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(fileContent, t)
 }
