843d1fe761ef272c3b6a62054547a6cba396355c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-20 12:14:49 +0100

    Build the tool in TestMain

--- cmd/task/main_test.go
+++ cmd/task/main_test.go
@@ -1,6 +1,12 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"testing"
+)
 
 var (
 	binName = "task-cli"
@@ -8,5 +14,17 @@ var (
 )
 
 func TestMain(m *testing.M) {
-	
+	fmt.Println("Building tool...")
+
+	// Append .exe extension to binName if the operating
+	// system that is running the test is windows.
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName)
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot build tool %s: %s", binName, err.Error())
+		os.Exit(1)
+	}
 }
